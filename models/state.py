ee31a475ce49a55d55f71bc956de536b6c777346
Caballero018 <4617@holbertonstudents.com>
2022-12-05 17:58:36 -0500

    Update State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+HBNB_TYPE_STORAGE = os.environ['HBNB_TYPE_STORAGE']
+
+class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     """ State class """
-    name = ""
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        from models import storage
+        from models.city import City
+        ls = []
+        objects_cities = storage.all(City)
+        for city in objects_cities.values():
+            if City.state_id == self.id:
+                ls.append(city)
+        return ls
