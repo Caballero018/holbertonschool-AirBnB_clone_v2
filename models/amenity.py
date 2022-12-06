7f5a334d037a147d8c1df721b8fa4429820caea9
Caballero018 <4617@holbertonstudents.com>
2022-12-05 21:34:46 -0500

    Update Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,10 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
+import os
 
 
-class Amenity(BaseModel, Base):
-    """ Amenity class to store amenity information """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+
+class Amenity(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="amenities"
+        )
+        
+    else:
+        name = ""
