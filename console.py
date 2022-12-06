b07a90dc9b377425515941a9a5772809efd3dbea
Caballero018 <4617@holbertonstudents.com>
2022-12-06 15:25:13 -0500

    Update Console

--- console.py
+++ console.py
@@ -219,7 +219,6 @@ class HBNBCommand(cmd.Cmd):
                 print_list.append(str(v))
         print(print_list)
 
-
     def help_all(self):
         """ Help information for the all command """
         print("Shows all objects, or all of a class")
@@ -324,6 +323,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
