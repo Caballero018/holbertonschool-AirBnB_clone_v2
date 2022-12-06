ae35b55d46f1564cf534ce6c096e6c3f2748828b
Caballero018 <4617@holbertonstudents.com>
2022-12-06 16:04:58 -0500

    Update User

--- models/user.py
+++ models/user.py
@@ -24,9 +24,9 @@ class User(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
             single_parent=True
         )
         reviews = relationship("Review", cascade="all,delete",
-            backref=backref("user", cascade="all,delete"),
-            passive_deletes=True,
-            single_parent=True)
+                               backref=backref("user", cascade="all,delete"),
+                               passive_deletes=True,
+                               single_parent=True)
     else:
         email = ''
         password = ''
