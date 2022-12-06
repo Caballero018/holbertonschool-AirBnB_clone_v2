3e9f0f86e96031138c69ebcbbaad987fddf7110e
Caballero018 <4617@holbertonstudents.com>
2022-12-05 19:05:55 -0500

    Update console

--- console.py
+++ console.py
@@ -211,14 +211,14 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all(args).items():
+            for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
             for k, v in storage.all().items():
                 print_list.append(str(v))
-
         print(print_list)
+
 
     def help_all(self):
         """ Help information for the all command """
