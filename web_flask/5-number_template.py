41ddc16297a71a3408b1ad51984b7fb94c461010
Caballero018 <4617@holbertonstudents.com>
2022-12-07 15:39:31 -0500

    Script that starts a Flask web application

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def text(text):
+    return "C {:s}".format(escape(text).replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def text_2(text):
+    return "Python {:s}".format(escape(text).replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(escape(n))
+
+
+@app.route('/number_template/<int:n>')
+def number(n):
+    return render_template('./templates/5-number.html', "{} is a number".format(escape(n)))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
