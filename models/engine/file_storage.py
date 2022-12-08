9c5a8125ad5876b72cb69e0ace6d3bb61f90210f
Caballero018 <4617@holbertonstudents.com>
2022-12-07 21:53:01 -0500

    reload() method for deserializing the JSON file to objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,6 @@ class FileStorage:
         if obj:
             key = ("{}.{}".format(type(obj).__name__, obj.id))
             del FileStorage.__objects[key]
+
+    def close(self):
+        self.reload()
