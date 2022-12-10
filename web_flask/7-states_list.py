2bebccdd0d577acf574fd8876ea3f739facc69d4
Caballero018 <4617@holbertonstudents.com>
2022-12-10 17:57:21 -0500

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,7 @@ app.url_map.strict_slashes = False
 def teardown_db(self):
     storage.close()
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def state_list():
     states = storage.all(State)
     return render_template('7-states_list.py', states = states)
