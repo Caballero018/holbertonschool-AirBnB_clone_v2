86c88c507fad2f3125aec09fbde68b417f3959dd
Caballero018 <4617@holbertonstudents.com>
2022-12-10 21:03:22 -0500

    List tables of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,26 +2,23 @@
 """Script that starts a Flask web application"""
 from flask import Flask, render_template
 from markupsafe import escape
+import sys
+sys.path.append("..")
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown_db(self):
     storage.close()
 
-
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Returns a HTML with states list"""
-    state_dict = storage.all(State)
-    ret_list = []
-    for state in state_dict.values():
-        ret_list.append(state)
-    return render_template('7-states_list.html', st_list=ret_list)
+def state_list():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
