9a99d26bae1da5ec5063fdc80037361a221a13d6
Caballero018 <4617@holbertonstudents.com>
2022-12-10 19:52:16 -0500

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,19 +9,19 @@ from models.state import State
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown_db(self):
     storage.close()
 
-@app.route('/states_list')
-def state_list():
-    stat= storage.all(State)
-    s = []
-    for s in stat:
-        s.append(stat)
-    return render_template('7-states_list.py', states=s)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Returns a HTML with states list"""
+    state_dict = storage.all(State)
+    ret_list = []
+    for state in state_dict.values():
+        ret_list.append(state)
+    return render_template('7-states_list.html', st_list=ret_list)
 
 
 if __name__ == '__main__':
