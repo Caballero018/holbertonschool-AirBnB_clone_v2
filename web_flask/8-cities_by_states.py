8d900387b11fd599ea2ad11b3406b10776319c81
Caballero018 <4617@holbertonstudents.com>
2022-12-11 19:36:14 -0500

    List States with cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""First flask module for HBNB"""
 from flask import Flask, render_template
-from markupsafe import escape
 from models import storage
 from models.state import State
-from models.city import City
-
-
+from os import getenv
 app = Flask(__name__)
+env = getenv('HBNB_TYPE_STORAGE')
 
 
 @app.teardown_appcontext
-def teardown_db(execute):
+def teardown_close(self):
+    "Closses sqlalchemy session"
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def state_list():
+def cities_by_state_list():
     """Returns a HTML with states list"""
-    state_dict = storage.all(State).values()
-    city_dict = storage.all(City).values()
-    return render_template('8-cities_by_states.html', states=state_dict, citie=city_dict)
+    state_list = storage.all(State).values()
+    return render_template('8-cities_by_states.html', st_list=state_list)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
